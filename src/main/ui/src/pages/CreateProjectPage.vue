9c1c0c1c5543a462050da6e40aa4566333d3e75c
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2023-10-29 12:53:45 +0200

    implemented create project frontend

---
+++ src/main/ui/src/pages/CreateProjectPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label>Project name</label>
+    <input v-model="projectName">
+    <button @click="submitCreateProject">Create</button>
+  </div>
+</template>
+
+<script>
+import {createProject} from "@/services/ProjectService";
+
+export default {
+  name: "CreateCoursePage",
+  data() {
+    return {
+      projectName: ""
+    }
+  },
+  methods: {
+    submitCreateProject() {
+      if (this.projectName !== "") {
+        createProject(this.projectName, () => {
+          this.$router.push({path: '/my-projects'});
+        });
+      } else {
+        alert("All field must be field");
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main/ui/src/services/ProjectService.js
+++ src/main/ui/src/services/ProjectService.js
@@ -1,12 +1,18 @@
 import getRequest from "@/services/getRequest";
+import postRequest from "@/services/postRequest";
 
 export async function getUsersProjects() {
     return getRequest('/api/projects/user-projects');
 }
+
 export async function getProjectsThatUserIn() {
     return getRequest('/api/projects/projects-user-in');
 }
 
 export async function getProjectContributions(projectId) {
     return getRequest('/api/projects/contributions?projectId='+projectId);
+}
+
+export async function createProject(projectName, action) {
+    return postRequest('/api/projects/create?name=' + projectName, action);
 }
